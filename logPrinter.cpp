797bacb58ef15bfe0acefaabaadaf92d2b1cd1b3
Afobaboa <gushind1984@gmail.com>
2024-09-26 18:31:49 +0300

    First version of logPrinter

---
+++ logPrinter.cpp
@@ -0,0 +1,330 @@
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "logPrinter.h"
+
+
+//----------------------------------------------------------------------------------------
+
+
+#define CONCATENATE(a, b) a ## b
+
+
+/**
+ * There is name of logs directory.
+ * If you will change name of directory,
+ * don't miss '/' at the end.
+ */
+#define LOG_DIRECTORY_NAME "logs/"
+
+
+/**
+ * There is name of log file. 
+ * You can change it yourself by
+ * LogSetFileName() before opening file.
+ */
+#define LOG_FILE_NAME LOG_DIRECTORY_NAME "log.txt"
+
+
+/**
+ * There is name of emergency log
+ * file. You can't change its name
+ * from other files.
+ */ 
+#define LOG_EMERGENCY_FILE_NAME LOG_DIRECTORY_NAME "emergencyLog.txt"
+
+
+//----------------------------------------------------------------------------------------
+
+
+/**
+ * There is pointer to log file.
+ */
+static FILE* logFile = NULL;
+
+
+/** 
+ * Codes used to set colors 
+ * for ColoredPrint() and 
+ * PrintColor().
+ */
+const char* COLOR_RED    = "\x1b[31m";
+const char* COLOR_GREEN  = "\x1b[32m";
+const char* COLOR_YELLOW = "\033[1;33m";
+const char* COLOR_WHITE  = "\033[1;37m";
+const char* COLOR_RESET  = "\x1b[0m";
+
+
+//----------------------------------------------------------------------------------------
+
+
+/**
+ * This function check if log is opened.
+ * 
+ * @return true if it's opened.
+ * @return false if it's closed.
+ */
+static bool IsLogOpen();
+
+
+/**
+ * This function print name of mode to log file.
+ * 
+ * @param logMode Mode of log note.
+ */
+static void LogPrintMode(FILE* outputFile, const logMode_t logMode);
+
+
+/**
+ * This function help you to handle
+ * hidden errors with using logs in 
+ * your program.
+ * 
+ * That prints message to special file
+ * "logs/emergencyLog.txt". You can't 
+ * change this path from other files.
+ * 
+ * @param message Message tha will be printed.
+ * @param place   Place that will be printed.
+ */
+static void LogEmergencyPrint(const Place place, const char* message);
+
+
+/**
+ * This function print time to outputFile
+ * without using '\n'.
+ * 
+ * @param outputFile File where time will be printed.
+ */
+static void LogPrintTime(FILE* outputFile);
+
+
+/**
+ * This function print place to outputFile
+ * without using '\n'.
+ * 
+ * @param outputFile File where place will be printed.
+ * @param place      Pointer to place which will b printed.
+ */
+static void LogPrintPlace(FILE* outputFile, const Place* place);
+
+
+/**
+ * This time print info about log message
+ * Template of printing:
+ * 
+ * logMode: place: date at time: '\n'.
+ * 
+ * @param outputFile File where oifo will be printed.
+ * @param logMod     Mode of loging.
+ * @param place      Pointer to place which will be printed.
+ */
+static void LogPrintInfo(FILE* outputFile, const logMode_t logMode, const Place* place);
+
+
+/** 
+ * Print needed color (used defines).
+ * 
+ * This function used to make 
+ * ColoredPrintf() more universal.
+ * 
+ * @param color Color you want to set.
+ */
+static void PrintColor(const color_t color);
+
+
+/**
+ * This directory create logs directory
+ * if it isn't exist. You can change 
+ * log directory name if you need.
+ */
+static void LogDirectoryCreate();
+
+
+//----------------------------------------------------------------------------------------
+
+
+void LogOpen(const Place place) 
+{
+    if (IsLogOpen())
+    {
+        LogEmergencyPrint(place, "You trying to open file that is already opened.");
+        return;
+    }
+
+    LogDirectoryCreate();
+    
+    logFile = fopen(LOG_FILE_NAME, "a");
+    setvbuf(logFile, NULL, _IONBF, 0);
+
+    fprintf(logFile, "\n\n\\* This log was created ");
+    LogPrintTime(logFile);
+    fprintf(logFile, "*\\\n\n");
+}
+
+
+void LogClose(const Place place)
+{
+    if (IsLogOpen())
+    {
+        fprintf(logFile, "\n\\* This log was closed ");
+        LogPrintTime(logFile);
+        fprintf(logFile, "*\\\n\n\n\n");
+                        
+        fclose(logFile);
+        logFile = NULL;
+    }
+    else 
+        LogEmergencyPrint(place, "You are trying to close log that is already closed.");
+}
+
+
+void LogPrint(logMode_t logMode, Place place, const char* message, ...) 
+{   
+    if (!IsLogOpen())
+        LogEmergencyPrint(place, "You are trying LOG_PRINT(), but you didn't "
+                                 "use LogOpen() before. Try to use it in main.cpp.");
+
+    va_list messageArgs;
+    va_start(messageArgs, message);
+
+    LogPrintInfo(logFile, logMode, &place);
+    vfprintf(logFile, message, messageArgs);
+    fprintf(logFile, "\n");
+
+    va_end(messageArgs);
+}
+
+
+int ColoredPrintf(color_t color, const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    PrintColor(color);
+    int result = vprintf(format, args);
+    printf("%s", COLOR_RESET);
+    
+    va_end(args);
+    return result;
+}
+
+
+//----------------------------------------------------------------------------------------
+
+
+static bool IsLogOpen()
+{
+    if (logFile == NULL)
+        return false;
+    
+    return true;
+}
+
+
+static void LogPrintMode(FILE* outputFile, const logMode_t logMode) 
+{
+    switch (logMode) 
+    {
+    case ERROR:
+        fprintf(outputFile, "ERROR: ");
+        break;
+
+    case WARNING:
+        fprintf(outputFile, "WARNING: ");
+        break;
+        
+    case INFO:
+        fprintf(outputFile, "INFO");
+        break;
+
+    default:
+        LogEmergencyPrint(GET_PLACE(), "WRONG LOG PRINT MODE");
+        break;
+    }
+}
+
+
+static void LogEmergencyPrint(const Place place, const char* message) 
+{
+    LogDirectoryCreate();
+
+    FILE* logEmergencyFile = fopen(LOG_EMERGENCY_FILE_NAME, "a");
+    setvbuf(logFile, NULL, _IONBF, 0);
+
+    LogPrintInfo(logEmergencyFile, ERROR, &place);
+    fprintf(logEmergencyFile, "\t%s\n", message);
+
+    fclose(logEmergencyFile);
+}
+
+
+static void PrintColor(const color_t color) {
+    switch(color) 
+    {
+    case GREEN:
+        printf("%s", COLOR_GREEN);
+        break;
+
+    case RED:
+        printf("%s", COLOR_RED);
+        break;
+
+    case YELLOW:
+        printf("%s", COLOR_YELLOW);
+        break;
+
+    case WHITE:
+        printf("%s", COLOR_WHITE);
+        break;
+
+    default: 
+        LogEmergencyPrint(GET_PLACE(), "Wrong color code.");
+        break;
+    }
+}
+
+
+static void LogPrintTime(FILE* outputFile)
+{
+    time_t myTime      = time(NULL);
+    tm*    myLocalTime = localtime(&myTime);
+
+    fprintf(outputFile, "%d.%d.%d at %d:%d:%d: ",
+            myLocalTime->tm_mday,  myLocalTime->tm_mon+1, myLocalTime->tm_year+1900,
+            myLocalTime->tm_hour,  myLocalTime->tm_min,   myLocalTime->tm_sec);
+}
+
+
+static void LogPrintPlace(FILE* outputFile, const Place* place)
+{
+    fprintf(outputFile, "%s: %s(): line %d: ",
+            place->file, place->function, place->line);
+}
+
+
+static void LogPrintInfo(FILE* outputFile, const logMode_t logMode, const Place* place)
+{
+    LogPrintMode(outputFile, logMode);
+    LogPrintTime(outputFile);
+    LogPrintPlace(outputFile, place);
+    fprintf(outputFile, "\n");
+}
+
+
+static void LogDirectoryCreate()
+{
+    struct stat myStat = {};
+    if (stat(LOG_DIRECTORY_NAME, &myStat) == -1) {
+        mkdir(LOG_DIRECTORY_NAME);
+    }
+}
+
+
+//----------------------------------------------------------------------------------------
+
+
+#undef LOG_DIRECTORY_NAME
+#undef LOG_FILE_NAME
+#undef LOG_EMERGENCY_FILE_NAME
---
+++ logPrinter.h
@@ -0,0 +1,212 @@
+/**
+ * @file 
+ * For correct working of rhis file
+ * use OPEN_LOG() at the beginning 
+ * of main() and close it by LOG_SLOSE()
+ * before return in main().
+ * 
+ * Use macroses for convinient
+ * using of logginng.
+ * 
+ * If you want to switch off loging
+ * without removing code just add
+ * 
+ * #define LOG_SWITCH_OFF
+ * 
+ * in main file, where you are
+ * opening log. 
+ * 
+ * DEFINE IT ONLY BEFORE INCLUDING
+ * ANY FILES, ESLE LOG WON'T 
+ * BE SWITCHED OFF!!!
+ */
+
+
+#ifndef LOG_PRINTER_H
+#define LOG_PRINTER_H
+
+
+//----------------------------------------------------------------------------------------
+
+
+/**
+ * This function automaticaly gets
+ * file, function and line information
+ * about plac where it was used and
+ * wraps that information into struct Place.
+ */
+#define GET_PLACE()                \
+    (struct Place)                 \
+    {                              \
+        .file     = __FILE__,      \
+        .function = __FUNCTION__,  \
+        .line     = __LINE__       \
+    }                              \
+
+
+#ifndef LOG_SWITCH_OFF
+
+
+/**
+ * This function works like LogPrint()
+ * but it's automaticaly place GET_PLACE()
+ * to arguments. 
+ * 
+ * That provides you more 
+ * convinient syntax.
+ */
+#define LOG_PRINT(logMode, ...)                  \
+    LogPrint(logMode, GET_PLACE(), __VA_ARGS__); \
+
+
+/**
+ * This definition provides you more
+ * convinient way to use logOpen().
+ * 
+ * Also this function will print
+ * date, time and place, where and
+ * when log was opened.
+ */
+#define LOG_OPEN()        \
+    LogOpen(GET_PLACE()); \
+
+
+/**
+ * This definition provides you more
+ * convinient way to use logClosw().
+ * 
+ * Also this function will print
+ * date, time and place, where and
+ * when log was closed
+ */
+#define LOG_CLOSE()        \
+    LogClose(GET_PLACE()); \
+
+
+#else
+
+
+/** 
+ * Where are switching off
+ * all log's defines.
+ */
+#define LOG_PRINT(logMode, ...) ;
+#define LOG_OPEN() ;
+#define LOG_CLOSE() ;
+
+
+#endif // LOG_SWITCHING_OFF
+
+
+//----------------------------------------------------------------------------------------
+
+
+
+/**
+ * This struct proides you convinient
+ * way to keep information about places in code.
+ */
+struct Place
+{
+    const char* file;        /**< Name of file.     */
+    const char* function;    /**< Name of function. */
+    const int   line;        /**< Number of line.   */
+};
+
+
+//----------------------------------------------------------------------------------------
+
+
+/**
+ * There are log modes which 
+ * used for notes in log records.
+ */
+enum LOG_MODES 
+{
+    ERROR,      /**< ERROR   */
+    WARNING,    /**< WARNING */
+    INFO        /**< INFO    */
+};
+typedef enum LOG_MODES logMode_t;
+
+
+/**
+ * This colores used for
+ * ColoredPrintf() to set
+ * needed color.
+ */
+enum COLORS {
+    GREEN,   /**< Green color. */
+    RED,     /**< Red color.   */
+    YELLOW,  /**< Yellow color */
+    WHITE    /**< White color  */
+};
+typedef enum COLORS color_t;
+
+
+//----------------------------------------------------------------------------------------
+
+
+/**
+ * This function open log file.
+ * If this file is already opened,
+ * this function print special message
+ * to "logs/emergencyLog.txt".
+ * 
+ * @param place Place that will be printed
+ *              in emergency log file.
+ */
+void LogOpen(const Place place);
+
+
+/**
+ * This function close log file.
+ * If this file is already closeed,
+ * this function print special message
+ * to "logs/emergencyLog.txt".
+ * 
+ * @param place Place that will be printed
+ *              in emergency log file.
+ */
+void LogClose(const Place place);
+
+
+/**
+ * This function print your message
+ * to "logs/log.txt" by default (you can
+ * chane log's output file by 
+ * LogSetFileName()). Also it prints
+ * Place where it was used.
+ * 
+ * @param logMode Note to log message.
+ * @param place   Place that will be printed.
+ * @param message Message with fomat like in printf(). 
+ *                Next arguments will be used in message
+ */
+void LogPrint(logMode_t logMode, Place place, const char* message, ...);
+
+
+/**
+ * This function is similary
+ * to printf(), but this function
+ * can print colored text.
+ * 
+ * @param color Color of text. You have
+ * to use something from enum COLORS.
+ * @param format Format of your text.
+ * You have to use printf() format
+ * 
+ * Other arguments used for subtitution
+ * to format. You have to use as many
+ * arguments as you us in format, like 
+ * in printf().
+ * 
+ * @return Count of used other arguments.
+ */
+int ColoredPrintf(color_t color, const char* format, ...);
+
+
+//----------------------------------------------------------------------------------------
+
+
+#endif // LOG_PRINTER_H
